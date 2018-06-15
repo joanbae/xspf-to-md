5faa9fb6f7da4babb79c338aa0867660cadd1fe6
joanbae <joanbae@snu.ac.kr>
2018-06-15 06:50:57 -0700

    Convert Playlist to Markdown & Save as a file named Playlist.md

--- src/main.ml
+++ src/main.ml
@@ -1,9 +1,9 @@
 open Xml
 module Tracks = Tracks.Tracks
 
-let x = parse_file "temp.xspf"
+let out_fileName = "playlist.md"
 
-let addTrack (t: xml) =
+let track (t: xml) =
   let getTextElement pos xdata =
     pcdata (List.hd (children (List.nth xdata pos)))
   in
@@ -16,16 +16,46 @@ let addTrack (t: xml) =
   Tracks.track title creator album img duration
 
 
-let addTracks (tlst: xml list) : Tracks.t =
-  List.fold_left (fun lst xml -> Tracks.add (addTrack xml) lst) [] tlst
+let makeTracks (tlst: xml list) : Tracks.t =
+  List.fold_left (fun lst xml -> Tracks.add (track xml) lst) [] tlst
 
 
-let parse x =
-  let tracklst = children (List.nth (children x) 1) in
-  let tracks = addTracks tracklst in
-  print_endline (Tracks.to_string tracks)
+let getTitle =
+  let t = Unix.localtime (Unix.time ()) in
+  let day, month, year =
+    (t.Unix.tm_mday, t.Unix.tm_mon, 1900 + t.Unix.tm_year - 2000)
+  in
+  Printf.sprintf "Playlist created on %02d/%02d/%02d\n" (month + 1) day year
 
 
-let main = parse x
+let parse input =
+  let xdata = parse_file input in
+  let tracklst = children (List.nth (children xdata) 1) in
+  makeTracks tracklst
+
+
+let markdown xlst = Markdown.make xlst
+
+let message = "Hello!"
+
+let write md toFile =
+  (* Write message to file *)
+  let oc = open_out toFile in
+  (* create or truncate file, return channel *)
+  Printf.fprintf oc "%s" md ;
+  (* write something *)
+  close_out oc ;
+  (* flush and close the channel *)
+  ()
+
+
+let main =
+  if Array.length Sys.argv <> 2 then (
+    prerr_endline "No InputFile was given" ;
+    exit 255 ) ;
+  let inputFile = Sys.argv.(1) in
+  let md = markdown (parse inputFile) in
+  write md out_fileName
+
 
 let () = main
