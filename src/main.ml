14adbaadd16d51aa60e9af70dab135e3c0aa0943
joanbae <joanbae@snu.ac.kr>
2018-06-15 23:32:36 -0700

    Changed time format & get console argument : inputFile, outputFile

--- src/main.ml
+++ src/main.ml
@@ -1,8 +1,6 @@
 open Xml
 module Tracks = Tracks.Tracks
 
-let out_fileName = "playlist.md"
-
 let track (t: xml) =
   let getTextElement pos xdata =
     pcdata (List.hd (children (List.nth xdata pos)))
@@ -12,8 +10,12 @@ let track (t: xml) =
   let creator = getTextElement 2 children in
   let album = getTextElement 3 children in
   let img = getTextElement 6 children in
-  let duration = getTextElement 7 children in
-  Tracks.track title creator album img duration
+  let milisec = int_of_string (getTextElement 7 children) in
+  let min, sec =
+    ( string_of_int (milisec / 60000)
+    , String.sub (string_of_int (milisec mod 60000)) 0 2 )
+  in
+  Tracks.track title creator album img (min ^ ":" ^ sec)
 
 
 let makeTracks (tlst: xml list) : Tracks.t =
@@ -50,9 +52,10 @@ let write md toFile =
 
 
 let main =
-  if Array.length Sys.argv <> 2 then (
+  if Array.length Sys.argv < 2 then (
     prerr_endline "No InputFile was given" ;
     exit 255 ) ;
+  let out_fileName = try Sys.argv.(2) ^ ".md" with _ -> "playlist.md" in
   let inputFile = Sys.argv.(1) in
   let md = markdown (parse inputFile) in
   write md out_fileName
